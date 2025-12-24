5c25e6480c1b43de2637ea35786578489e0c83c6
Thoko0 <69698501+Thoko0@users.noreply.github.com>
2025-12-24 10:13:36 +0200

    schemas

--- LivestockTracker/backend/app/schemas.py
+++ LivestockTracker/backend/app/schemas.py
@@ -2,13 +2,13 @@ from pydantic import BaseModel
 
 class TrackerDataSchema(BaseModel):
     device_id: str
-    lat: float
-    lon: float
+    latitude: float
+    longitude: float
     speed: float
     distance: float
     behavior: float
     timestamp: str
-    
+
 class UserLogin(BaseModel):
     username: str
     password: str
