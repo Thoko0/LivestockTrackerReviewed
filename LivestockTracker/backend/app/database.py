dcb091794b2ce578b5a1b990916e2a694a2a11f4
Thoko0 <69698501+Thoko0@users.noreply.github.com>
2026-01-14 18:25:25 +0200

    moved db to supabase

--- LivestockTracker/backend/app/database.py
+++ LivestockTracker/backend/app/database.py
@@ -2,17 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 from dotenv import load_dotenv
+from supabase import create_client
+
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
-engine = create_engine(DATABASE_URL, echo=True)
 
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-Base = declarative_base()
