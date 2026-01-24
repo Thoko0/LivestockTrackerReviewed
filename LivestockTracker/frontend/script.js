cfb7d7c960b07797a2588bbc48e8778f02f1b586
Thoko0 <69698501+Thoko0@users.noreply.github.com>
2026-01-24 22:43:21 +0200

    login page redirection

--- LivestockTracker/frontend/script.js
+++ LivestockTracker/frontend/script.js
@@ -1,3 +1,13 @@
+// ==========================
+// Login page entry
+// ==========================
+const token = localStorage.getItem("access_token");
+
+if (!token) {
+    window.location.href = "/login.html";
+}
+
+
 // ===========================
 // CONFIG
 // ===========================
